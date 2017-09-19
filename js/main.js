c10a46fd78bbaf3e6de51a3e2611895da4416685
megasrd <reganjduthie@gmail.com>
2017-09-19 11:20:29 +0200

    City Data Fixed
    Changed method of storing data and setting it

--- js/main.js
+++ js/main.js
@@ -3,27 +3,54 @@
 
 
 
+//Stylesheet Switch--End
+
+
+
 
 //City Data 
 
 var currentCity;
 
-var city1 = {cityName : "Cape Town, South Africa", population : "1.5m", dotw : "Monday", weather_temp : "12", weather_type : "Sunny", cityImage : "../"};
+var city_data;
 
-var city2 = {cityName : "Tokyo, Japan", population : "1.5m", dotw : "Monday", weather_temp : "12", weather_type : "Sunny", cityImage : "../"};
+var city1 = {cityName : "Cape Town, South Africa", population : "3.74m", dotw : "Monday", weather_temp : "22", weather_type : "Sunny", cityImage : "../"};
 
-var city3 = {cityName : "New York, USA", population : "1.5m", dotw : "Monday", weather_temp : "12", weather_type : "Sunny", cityImage : "../"};
+var city2 = {cityName : "Tokyo, Japan", population : "13.62m", dotw : "Tuesday", weather_temp : "10", weather_type : "Cloudy", cityImage : "../"};
 
-var city4 = {cityName : "Munich, Germany", population : "1.5m", dotw : "Monday", weather_temp : "12", weather_type : "Sunny", cityImage : "../"};
+var city3 = {cityName : "New York, USA", population : "8.53m", dotw : "Wednesday", weather_temp : "7", weather_type : "Rainy", cityImage : "../"};
+
+var city4 = {cityName : "Munich, Germany", population : "1.37m", dotw : "Thursday", weather_temp : "9", weather_type : "Windy", cityImage : "../"};
 
 
-var city_data = new Vue({
-    el: '.location-title',
+function initiate_cityData() {
     
-    data: {
-        city_name: city1.cityName
+    
+    if (currentCity == null) {
+        set_cityData(1);
+          
+          city_data = new Vue({
+    
+              el: '.city-data',
+
+              data: {
+                  city_name: currentCity.cityName,
+                  city_pop: currentCity.population,
+                  city_dotw: currentCity.dotw,
+                  city_weatherTemp: currentCity.weather_temp,
+                  city_weatherType: currentCity.weather_type
+              }
+          });
+      }
+    
+    if (city_data != null) {
+            city_data.city_name = currentCity.cityName;
+            city_data.city_pop = currentCity.population;
+            city_data.city_dotw = currentCity.dotw;
+            city_data.city_weatherTemp = currentCity.weather_temp;
+            city_data.city_weatherType = currentCity.weather_type;
     }
-});
+}
 
 
 function set_cityData(caseNum) {
@@ -32,22 +59,22 @@ function set_cityData(caseNum) {
             
         case 1:
             currentCity = city1;
-            city_data.city_name = city1.cityName;
+            initiate_cityData();
             break;
         
         case 2:
             currentCity = city2;
-            city_data.city_name = city2.cityName;
+            initiate_cityData();
             break;
             
         case 3:
             currentCity = city3;
-            city_data.city_name = city3.cityName;
+            initiate_cityData();
             break;
             
         case 4:
             currentCity = city4;
-            city_data.city_name = city4.cityName;
+            initiate_cityData();
             break; 
     }
     
